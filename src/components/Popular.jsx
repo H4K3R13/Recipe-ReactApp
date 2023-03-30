9523898125ae89958cb32ab93a73a380d6e32e92
H4K3R13 <arjunmundamani@gmail.com>
2023-03-30 20:44:35 +0530

    Update Popular.jsx

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -12,30 +12,40 @@ function Popular() {
   },[]);
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-      );
-    const data = await api.json();
-    setPopular(data.recipes);
-  }
+
+    const check = localStorage.getItem('popular');
+    if(check){
+      setPopular(JSON.parse(check))
+    }
+    else{
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+        );
+      const data = await api.json();
+      localStorage.setItem('popular',JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+      console.log(data)
+    }
+  };
   return (
     <div>
         <Wrapper>
           <h3>Popular Picks</h3>
           <Splide options={{
-            perPage:4,
+            perPage:2,
             arrows: false,
             pagination: false,
             drag: 'free' ,
-            gap: '5rem',
+            gap: '1.5rem',
           }}
           >
           {popular.map((recipe) => {
             return(
-              <SplideSlide>
+              <SplideSlide key={recipe.id}>
               <Card>
                 <p>{recipe.title}</p>
                 <img src={recipe.image} alt={recipe.title} />
+                <Gradient/>
               </Card>
               </SplideSlide>
             );
@@ -54,9 +64,38 @@ const Card = styled.div`
   min-height: 25rem ;
   border-radius: 2rem ;
   over-flow:hidden;
+  position: relative;
 
   img{
-    border-radius: 2rem;
+    border-radius: 3rem;
+    position: absolute;
+    left:0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+  p{
+    position:absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform:translate(-50%,0%);
+    color:white;
+    text-align:center;
+    font-weight:600;
+    font-size:1rem;
+    height:40%;
+    display:flex;
+    justify-contents:center;
+    align-items:center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index:3;
+  position:absolute;
+  width:100%;
+  height:100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
 `;
 export default Popular
